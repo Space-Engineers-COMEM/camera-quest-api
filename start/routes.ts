f4eec39ac570268df67fa32a4968eb86d40146ce
Satche <info@satche.ch>
2022-04-01 13:53:20 +0200

    Config mysql database

--- start/routes.ts
+++ start/routes.ts
@@ -18,8 +18,9 @@
 |
 */
 
-import Route from '@ioc:Adonis/Core/Route'
+import Database from '@ioc:Adonis/Lucid/Database';
+import Route from '@ioc:Adonis/Core/Route';
 
 Route.get('/', async () => {
-  return { hello: 'world' }
-})
+  return Database.from('users').select('*');
+});
