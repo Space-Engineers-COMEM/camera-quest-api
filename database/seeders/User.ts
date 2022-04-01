1bf655b4837f984502de3795dcdbbd105b35493f
Satche <info@satche.ch>
2022-04-01 14:55:28 +0200

    Add seeder example

---
+++ database/seeders/User.ts
@@ -0,0 +1,17 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder';
+import User from 'App/Models/User';
+
+export default class UserSeeder extends BaseSeeder {
+  public async run() {
+    await User.createMany([
+      {
+        name: 'Denis',
+        password: 'password',
+      },
+      {
+        name: 'John Doe',
+        password: 'password',
+      },
+    ]);
+  }
+}
