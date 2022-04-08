97f6a9ca914edc246738a00853bc8ffb066f559b
NF01 <noemail@noemail>
2022-04-08 11:14:32 +0200

    Update POIController.poi

--- app/Controllers/Http/PoisController.ts
+++ app/Controllers/Http/PoisController.ts
@@ -1,6 +1,8 @@
 import Poi from 'App/Models/Poi';
 import TranslationModel from 'App/Models/Translation';
+import Resource from 'App/Models/Resource';
 import PoisValidator from 'App/Validators/PoisValidator';
+import Resources from 'Database/migrations/1649074790208_resources';
 
 export default class PoisController {
   public async index() {
@@ -24,9 +26,16 @@ export default class PoisController {
 
   public async poi({ request, response }) {
     const params = request.params();
+
     try {
-      const poiDB = await Poi.findOrFail(params.id);
-      const poiFinal = poiDB.$attributes;
+      // Search in DB for the POI and error handling
+      const poiDB = await Poi.query().where('id', params.id);
+      if (poiDB.length <= 0)
+        return response.badRequest({
+          message: `The POI with the id '${params.id}' is not existant`,
+        });
+
+      // Search in DB for the translation and error handling
       const translate = await TranslationModel.query()
         .where('id_lang', params.lang)
         .where('id_poi', params.id);
@@ -34,14 +43,24 @@ export default class PoisController {
         return response.badRequest({
           message: `Translation in '${params.lang}' not available`,
         });
+
+      // Search in DB for the ressources and error handling
+      const resources = await Resource.query().where('id_poi', params.id);
+      if (resources.length <= 0)
+        return response.badRequest({
+          message: `Resource for the POI with the id '${params.id}' is not available`,
+        });
+
+      //creation the response sended to the client
       const responseToSend = {
-        poi: poiFinal,
+        poi: poiDB,
         translations: translate,
+        resources: resources,
       };
       return response.ok(responseToSend);
     } catch (error) {
-      return response.badRequest({
-        message: 'Not able to give you the POI please check the id',
+      response.status(500).send({
+        message: `Internal problem server fetching in the database for the POI`,
       });
     }
   }
