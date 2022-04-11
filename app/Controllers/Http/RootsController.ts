29c4f3ecdffe72853b8b2c6f26f0915b5a022397
NF01 <noemail@noemail>
2022-04-11 13:45:10 +0200

    Change LINK env variable to BASEURL

--- app/Controllers/Http/RootsController.ts
+++ app/Controllers/Http/RootsController.ts
@@ -2,7 +2,7 @@ import Env from '@ioc:Adonis/Core/Env';
 import { RootInfo } from '../../../types/SharpObjects';
 
 export default class RootsController {
-  private link: string = Env.get('LINK', 'http://localhost:3333/');
+  private link: string = Env.get('BASEURL');
   public async root() {
     const rootInfo: RootInfo = {
       message: 'Links to begin in this RESTfull API, have a nice trip :)',
