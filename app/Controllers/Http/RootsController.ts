a785a301cf4f7c2177785e8f51f6d6e803c92a82
Luka Najjar <lukanajjar@hotmail.com>
2022-04-12 15:27:59 +0200

    Update .env

--- app/Controllers/Http/RootsController.ts
+++ app/Controllers/Http/RootsController.ts
@@ -2,12 +2,14 @@ import Env from '@ioc:Adonis/Core/Env';
 import { RootInfo } from '../../../types/SharpObjects';
 
 export default class RootsController {
-  private link: string = Env.get('BASEURL');
+  private url: string = Env.get('BASE_URL');
+  private port: number = Env.get('PORT');
   public async root() {
+    let link = `${this.url}:${this.port}/`;
     const rootInfo: RootInfo = {
       message: 'Links to begin in this RESTfull API, have a nice trip :)',
-      admin_link: `${this.link}users`,
-      pois_link: `${this.link}pois`,
+      admin_link: `${link}users`,
+      pois_link: `${link}pois`,
     };
     return rootInfo;
   }
