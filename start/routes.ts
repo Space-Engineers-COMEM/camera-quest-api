c854b5a366854e92702ab5ee0cfba5b8b67d4917
Luka Najjar <lukanajjar@hotmail.com>
2022-04-13 16:27:29 +0200

    Change pun again

--- start/routes.ts
+++ start/routes.ts
@@ -65,5 +65,5 @@ Route.resource('/translations', 'TranslationsController')
 Route.post('/pois/prediction/debug', 'PoisController.getPredictionDebug');
 
 Route.get('*', () => {
-  return 'Toutes les routes mènent à Denis, mais pas celle-ci!';
+  return 'Toutes les routes mènent à Denis, mais pas celle-ci! ¯\\_(ツ)_/¯ ';
 });
