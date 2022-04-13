e9c5798770e31e1548d564fa409aed30ef5e80ea
Luka Najjar <lukanajjar@hotmail.com>
2022-04-13 13:32:19 +0200

    Add AuthController doc

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,4 +1,10 @@
 export default class AuthController {
+  /**
+   * It attempts to authenticate the user using the email and password provided in the request body,
+   * and if successful, returns the token
+   * @param  - auth: The auth module.
+   * @returns The token is being returned.
+   */
   public async login({ auth, request, response }) {
     const email = request.input('email');
     const password = request.input('password');
@@ -13,6 +19,12 @@ export default class AuthController {
     }
   }
 
+  /**
+   * It checks if the token is in the database, if it is, it logs the user out and returns a revoke:
+   * true object, otherwise it returns a revoke: false object
+   * @param  - auth - The auth object
+   * @returns The response is being returned.
+   */
   public async logout({ auth, response }) {
     try {
       //check if the token is in the database
